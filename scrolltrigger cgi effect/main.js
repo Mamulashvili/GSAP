3a641ddfca55b4639dd7c93e73d9348e9b4fe211
Shota Mamulashvili <mamulashvili13@gmail.com>
2022-01-26 17:44:42 +0400

    cgi multiple layers

--- scrolltrigger cgi effect/main.js
+++ scrolltrigger cgi effect/main.js
@@ -42,5 +42,15 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     });
 
+    const layers = gsap.utils.toArray('.multiple-layers span');
+    layers.map((el, ind) => {
+        gsap.to(layers, {
+            width: '100%',
+            ease: 'none',
+            repeat: -1,
+            yoyo: true,
+            stagger: 1,
+        })
+    })
 
 })
