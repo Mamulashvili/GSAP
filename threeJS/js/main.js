2f03cb94865a88b5a1b7f8f8148af1735ff95d0a
Shota Mamulashvili <mamulashvili13@gmail.com>
2022-01-27 17:15:05 +0400

    basic setup

---
+++ threeJS/js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let scene, camera, renderer;
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xdddddd);
+
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+
+    hlight = new THREE.AmbientLight(0x404040, 100);
+    scene.add(hlight);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+})
